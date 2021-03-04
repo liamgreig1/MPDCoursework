1ab96eb73988bf718b37d3be46be0938762596e9
liamgreig1 <liam.greig1998@googlemail.com>
2021-03-04 23:33:25 +0000

    Used AsyncTask method to retrieve data from source

--- app/src/main/java/org/liamgreig/gcu/mpdcoursework/MainActivity.java
+++ app/src/main/java/org/liamgreig/gcu/mpdcoursework/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,16 +22,23 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView rawDataDisplay;
     private RecyclerView rvEarthquake;
     private EarthquakeAdapter adapter;
     private String result;
     private String url1 = "";
     private String urlSource = "http://quakes.bgs.ac.uk/feeds/MhSeismology.xml";
     ArrayList<EarthquakeClass> earthquakeList = new ArrayList<>();
+    Context mainContext = this;
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,67 +47,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Log.e("MyTag", "in onCreate");
         // Set up the raw links to the graphical components
-//        rawDataDisplay = (TextView) findViewById(R.id.rawDataDisplay);
-
+        rvEarthquake = findViewById(R.id.rvEarthquake);
         Button startButton = findViewById(R.id.startButton);
         startButton.setOnClickListener(this);
-        Log.e("MyTag", "after startButton");
-//        EarthquakeClass anQuake = new EarthquakeClass("Test","Test","Test","Test","Test","Test","Test","Test","Test");
-//        earthquakeList.add(anQuake);
 
-
-        // More Code goes here
+        periodicUpdate.run();
     }
+
+
 
     @Override
     public void onClick(View v) {
         Log.e("MyTag", "in onClick");
-        startProgress();
 
-        rvEarthquake = findViewById(R.id.rvEarthquake);
-        rvEarthquake.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new EarthquakeAdapter(earthquakeList);
-        rvEarthquake.setAdapter(adapter);
         Log.e("MyTag", "after startProgress");
     }
 
-    public void startProgress() {
-        // Run network access on a separate thread;
-        new Thread(new Task(urlSource)).start();
-
-
-    } //
-
-    // Need separate thread to access the internet resource over network
-    // Other neater solutions should be adopted in later iterations.
-    private class Task implements Runnable {
-
-        private String url;
-
-        public Task(String aurl) {
-            url = aurl;
-        }
-        @Override
+    private Runnable periodicUpdate = new Runnable () {
         public void run() {
+            // scheduled another events to be in 1 Hour
+            AsyncTask<Void, Integer, Void> runTask = new GetXMLAsyncTask();
+            handler.postDelayed(periodicUpdate, 10*360000);
+            Log.e("MyTag", "Update");
+            runTask.execute();
+        }
+    };
+
+
+    private class GetXMLAsyncTask extends AsyncTask<Void, Integer, Void>
+    {
+        @Override
+        protected Void doInBackground(Void... params)
+        {
             URL aurl;
             URLConnection yc;
             BufferedReader in = null;
             String inputLine = "";
 
-
             Log.e("MyTag", "in run");
 
             try {
                 Log.e("MyTag", "in try");
-                aurl = new URL(url);
+                aurl = new URL(urlSource);
                 yc = aurl.openConnection();
                 in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
                 Log.e("MyTag", "after ready");
-                //
-                // Throw away the first 2 header lines before parsing
-                //
-                //
-                //
+
                 while ((inputLine = in.readLine()) != null) {
                     if (result != null) {
                         result = result + inputLine;
@@ -112,20 +109,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } catch (IOException ae) {
                 Log.e("MyTag", "ioexception in run");
             }
+            return null;
+        }
 
-            //
-            // Now that you have the xml data you can parse it
-            //
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+            Toast.makeText(MainActivity.this,"Invoke onPostExecute()", Toast.LENGTH_SHORT).show();
+            rvEarthquake.setLayoutManager(new LinearLayoutManager(mainContext));
+            adapter = new EarthquakeAdapter(earthquakeList);
+            rvEarthquake.setAdapter(adapter);
 
-            // Now update the TextView to display raw XML data
-            // Probably not the best way to update TextView
-            // but we are just getting started !
-            MainActivity.this.runOnUiThread(new Runnable() {
-                public void run() {
-                    Log.d("UI thread", "I am the UI thread");
-//                    rawDataDisplay.setText(earthquakeList.toString());
-                }
-            });
         }
     }
+
 }
